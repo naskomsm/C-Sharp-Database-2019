0aa08ebb1852b8aa3416ccdf2d6570fcece0e93c
Atanas <naskokolev00@gmail.com>
2019-10-07 11:03:00 +0300

    finished some tasks from exam

--- MS-SQL-2019/Databases-MSSQL-ServerExam-16-Apr-2019.sql
+++ MS-SQL-2019/Databases-MSSQL-ServerExam-16-Apr-2019.sql
@@ -140,10 +140,28 @@ SELECT CONCAT(p.FirstName, ' ',p.LastName) AS [Full Name],pl.Name AS [Plane Name
 	JOIN LuggageTypes AS lt ON l.LuggageTypeId = lt.Id
 	ORDER BY [Full Name],[Plane Name],Origin,Destination,[Luggage Type]
 
-SELECT p.FirstName,p.LastName,f.Destination,MAX(t.Price) AS [Price]
-	FROM Passengers AS p
-	JOIN Tickets AS t ON p.Id = t.PassengerId
-	JOIN Flights AS f ON t.FlightId = f.Id
-	GROUP BY p.FirstName,p.LastName,f.Destination
-	ORDER BY Price DESC,FirstName,LastName,Destination
+SELECT k.FirstName, k.LastName, k.Destination, k.Price
+	  FROM (
+		SELECT p.FirstName, p.LastName, f.Destination, t.Price,
+			   DENSE_RANK() OVER(PARTITION BY p.FirstName, p.LastName ORDER BY t.Price DESC) As PriceRank
+		  FROM Passengers AS p
+		  JOIN Tickets AS t ON t.PassengerId = p.Id
+		  JOIN Flights AS f ON f.Id = t.FlightId
+	  ) AS k 
+	  WHERE k.PriceRank = 1
+	  ORDER BY k.Price DESC, k.FirstName, k.LastName, k.Destination
 
+
+SELECT fl.Destination, COUNT(t.FlightId) AS [FilesCount]
+	FROM Tickets AS t
+	FULL OUTER JOIN Flights AS fl ON fl.Id = t.FlightId
+	GROUP BY fl.Destination
+	ORDER BY [FilesCount] DESC, fl.Destination
+
+SELECT pl.[Name], pl.Seats, COUNT(t.Id) AS [Passangers Count]
+	FROM Planes AS pl
+	LEFT JOIN Flights AS fl ON pl.Id = fl.PlaneId
+	LEFT JOIN Tickets AS t ON fl.Id = t.FlightId
+	LEFT JOIN Passengers AS p ON p.Id = t.PassengerId
+	GROUP BY pl.[Name], pl.Seats
+	ORDER BY [Passangers Count] DESC, pl.[Name], pl.Seats
--- MS-SQL-2019/QueryGeographyDB.sql
+++ MS-SQL-2019/QueryGeographyDB.sql
@@ -69,7 +69,7 @@ SELECT TOP(5) c.CountryName, MAX(p.Elevation) AS HighestPeakElevation, MAX(r.Len
 SELECT TOP(5) k.CountryName,k.[Highest Peak Name],k.[Highest Peak Elevation],k.Mountain
 	FROM
 		(SELECT c.CountryName,
-			 ISNULL(p.PeakName,'(no highest rank)') AS [Highest Peak Name],
+			 ISNULL(p.PeakName,'(no highest peak)') AS [Highest Peak Name],
 			 ISNULL(p.Elevation,0) AS [Highest Peak Elevation],
 			 ISNULL(m.MountainRange,'(no mountain)') AS [Mountain],
 			 DENSE_RANK() OVER (PARTITION BY c.CountryName ORDER BY p.Elevation DESC) AS ElevationRank
