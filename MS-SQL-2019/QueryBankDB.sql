e513b9b1fc077a02eeef4eb51c5a599ef826ded7
Atanas <naskokolev00@gmail.com>
2019-10-10 14:50:45 +0300

    fixed task

--- MS-SQL-2019/QueryBankDB.sql
+++ MS-SQL-2019/QueryBankDB.sql
@@ -20,6 +20,27 @@ END
 
 EXEC dbo.usp_GetHoldersWithBalanceHigherThan 1000
 
+--
+
+CREATE FUNCTION ufn_CalculateFutureValue(@sum DECIMAL,@yearlyInterestRate FLOAT,@numberOfYears INT)
+RETURNS DECIMAL(18,4)
+AS
+BEGIN
+	DECLARE @futureValue DECIMAL(18,4)
+	SET @futureValue = @sum * (POWER((1 + @yearlyInterestRate),@numberOfYears))
+	RETURN @futureValue
+END
 
 
+--
 
+CREATE PROC usp_CalculateFutureValueForAccount(@accountId INT, @yearlyInterestRate FLOAT)
+AS
+BEGIN
+	SELECT @accountId AS [Account Id], ah.FirstName,ah.LastName, a.Balance, dbo.ufn_CalculateFutureValue(a.Balance, @yearlyInterestRate,5) AS [Balance in 5 years]
+		FROM Accounts AS a
+		JOIN AccountHolders AS ah ON a.AccountHolderId = ah.Id
+		WHERE a.Id = @accountId
+END
+
+EXEC usp_CalculateFutureValueForAccount 1, 0.1
