d2420ae3f2670a5a696aa568236deb7a4e89e588
Atanas Kolev <naskokolev00@gmail.com>
2019-11-23 00:22:20 +0200

    finished everything

---
+++ EntityFramework/Exams/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/DataProcessor/ExportDtos/SongExportDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace MusicHub.DataProcessor.ExportDtos
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Song")]
+    public class SongExportDTO
+    {
+        [XmlElement("SongName")]
+        public string Name { get; set; }
+
+        [XmlElement("Writer")]
+        public string WriterName { get; set; }
+
+        [XmlElement("Performer")]
+        public string PerformerName { get; set; }
+
+        [XmlElement("AlbumProducer")]
+        public string AlbumProducerName { get; set; }
+
+        [XmlElement("Duration")]
+        public string Duration { get; set; }
+    }
+}
---
+++ EntityFramework/Exams/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/DataProcessor/ImportDtos/PerformerImputDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace MusicHub.DataProcessor.ImportDtos
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Performer")]
+    public class PerformerImputDTO
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Invalid performer first name length")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Invalid performer last name length")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(18, 70)]
+        public int Age { get; set; }
+
+        [Required]
+        [Range(0, double.PositiveInfinity)]
+        public decimal NetWorth { get; set; }
+
+        [XmlArray("PerformersSongs")]
+        public SongIdDTO[] PerformerSongs { get; set; }
+
+    }
+}
